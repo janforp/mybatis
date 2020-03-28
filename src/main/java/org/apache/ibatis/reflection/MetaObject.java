f4da0d2be99cb56a3cdc42cbaa737f8409899517
zhucj <zhucj@servyou.com.cn>
2020-03-28 12:16:10 +0800

    PropertyTokenizer

--- src/main/java/org/apache/ibatis/reflection/MetaObject.java
+++ src/main/java/org/apache/ibatis/reflection/MetaObject.java
@@ -1,19 +1,3 @@
-/*
- *    Copyright 2009-2012 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package org.apache.ibatis.reflection;
 
 import org.apache.ibatis.reflection.factory.ObjectFactory;
@@ -49,7 +33,6 @@ public class MetaObject {
         this.originalObject = object;
         this.objectFactory = objectFactory;
         this.objectWrapperFactory = objectWrapperFactory;
-
         if (object instanceof ObjectWrapper) {
             //如果对象本身已经是ObjectWrapper型，则直接赋给objectWrapper
             this.objectWrapper = (ObjectWrapper) object;
@@ -193,5 +176,4 @@ public class MetaObject {
     public <E> void addAll(List<E> list) {
         objectWrapper.addAll(list);
     }
-
 }
